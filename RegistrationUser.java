f9dbfe2f0b89dca2c449646dd94a267902fc1bb1
aleksandarvalencia <32481910+aleksandarvalencia@users.noreply.github.com>
2017-11-07 16:39:40 +0100

    Add files via upload

---
+++ RegistrationUser.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projects.angularcrud.model;
+
+/**
+ *
+ * @author BD101009
+ */
+public class RegistrationUser {
+    
+    private boolean enabled;
+     
+    public RegistrationUser() {
+        super();
+        this.enabled=false;
+    }
+    
+}
